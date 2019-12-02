964b0c7cf88acd3caafc18952975b8bde80abeb7
Joel Seng <joel29dec@gmail.com>
2019-12-02 15:22:44 -0600

    first

---
+++ 2.js
@@ -0,0 +1,16 @@
+let arr = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,2,23,10,27,1,27,5,31,1,31,6,35,1,6,35,39,2,39,13,43,1,9,43,47,2,9,47,51,1,51,6,55,2,55,10,59,1,59,5,63,2,10,63,67,2,9,67,71,1,71,5,75,2,10,75,79,1,79,6,83,2,10,83,87,1,5,87,91,2,9,91,95,1,95,5,99,1,99,2,103,1,103,13,0,99,2,14,0,0]
+
+for (let i = 0; i < arr.length; i+=4) {
+    if(arr[i] === 99) break
+    let registerOne = arr[arr[i + 1]]
+    let registerTwo = arr[arr[i + 2]]
+    let outputIndex = arr [i + 3]
+    if( arr[i] === 1){
+        arr[outputIndex] = registerOne + registerTwo
+    }
+    else if (arr[i] === 2){
+        arr[outputIndex] = registerOne * registerTwo
+    }
+}
+console.log(arr[0])
+
